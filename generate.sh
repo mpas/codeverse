328c51cabb6c1e66ec04b7529ca18665c9782513
Marco Pas <marco.pas@gmx.net>
2015-11-19 13:01:58 +0100

    update

--- generate.sh
+++
@@ -1,3 +0,0 @@
-rm -rf public
-rm -rf /Users/marco/Dropbox/blog/mpas.github.io/*
-hugo -d /Users/marco/Dropbox/blog/mpas.github.io -t hugo-multi-bootswatch
--- run.sh
+++
@@ -1,2 +0,0 @@
-rm -rf public
-hugo server --theme=hugo-multi-bootswatch --buildDrafts --watch
